9be4ec94f5f907124c4e15b4126dfdced92bea71
Merrick Minogue <yt7i0aoi@students.codeinstitute.net>
2025-01-21 11:01:22 +0000

    Update base.html, add links to css and js files, add navbar and sidebar

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Sidenav Initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
